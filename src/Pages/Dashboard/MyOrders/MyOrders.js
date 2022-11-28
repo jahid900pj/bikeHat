091c278a49e1a3487016e7b50ea311cef0554aa6
jahidhasan234031 <jahidhasan234031@mail.com>
2022-11-28 16:41:57 +0600

    my orders done

--- src/Pages/Dashboard/MyOrders/MyOrders.js
+++ src/Pages/Dashboard/MyOrders/MyOrders.js
@@ -1,9 +1,76 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../Context/AuthProvider';
+import Loading from '../../Shared/Loading/Loading';
 
 const MyOrders = () => {
-    return (
-        <div>
+    const { user } = useContext(AuthContext)
 
+    const uri = `http://localhost:5000/bikeBooking?email=${user?.email}`
+
+    const { data: bookings = [], isLoading } = useQuery({
+        queryKey: ['bookings', user?.email],
+        queryFn: async () => {
+            const res = await fetch(uri, {
+                // headers: {
+                //     authorization: `bearer ${localStorage.getItem('accessToken')}`
+                // }
+            })
+            const data = await res.json()
+            console.log(data)
+            return data
+        }
+    })
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
+    return (
+        <div className='mt-10'>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Bike Name</th>
+                            <th>Price</th>
+                            <th>location</th>
+                            <th>Seller</th>
+                            <th>Payment</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            bookings.map((booking, i) =>
+                                <tr key={booking._id} className="hover">
+                                    <th>{i + 1}</th>
+                                    <td>{booking.bikeName}</td>
+                                    <td>{booking.price}</td>
+                                    <td>{booking.MeetingLocation}</td>
+                                    <td>{booking.sellerName}</td>
+                                    <td> <button className="btn btn-sm btn-primary">Pay</button></td>
+                                    {/* <td>{booking.patientName}</td>
+                                    <td>{booking.treatment}</td>
+                                    <td>{booking.appointmentDate}</td>
+                                    <td>{booking.slot}</td>
+                                    <td>
+                                        {
+                                            booking.price && !booking.paid &&
+                                            <Link to={`/dashboard/payment/${booking._id}`}>
+                                                <button className="btn btn-sm btn-primary">Pay</button>
+                                            </Link>
+                                        }</td> */}
+                                </tr>
+                            )
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
